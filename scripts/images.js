1da8027ddc8aa2d824ac2860a433c4dd91678f8e
v0rvex <105231617+v0rvex@users.noreply.github.com>
2024-10-11 11:37:09 +0500

    Add files via upload

--- scripts/images.js
+++ scripts/images.js
@@ -1,37 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const imagesContainer = document.getElementById('images_container');
-    const loadMoreButton = document.getElementById('load_more');
-    const images = []; // Массив ссылок на изображения
-    let currentIndex = 0;
-    const imagesPerLoad = 30; // Количество изображений для загрузки за один раз
-  
-    // Добавляем ссылки на изображения из папки 'Images'
-    for (let i = 1; i <= 90; i++) {
-      images.push(`Images/image${i}.png`); // Предполагается, что изображения названы 'image1.jpg', 'image2.jpg' и т.д.
-    }
-  
-    function loadImages() {
+  const imagesContainer = document.getElementById('images_container');
+  const loadMoreButton = document.getElementById('load_more');
+  let images = []; // Массив ссылок на изображения, который мы заполним из JSON
+  let currentIndex = 0;
+  const imagesPerLoad = 30; // Количество изображений для загрузки за один раз
+  const jsonUrl = 'https://raw.githubusercontent.com/v0rvex/il-art/refs/heads/main/image_list.json'; // URL к JSON файлу
+
+  // Функция для загрузки JSON файла с изображениями
+  function loadJson() {
+      return fetch(jsonUrl)
+          .then(response => response.json())
+          .then(data => {
+              images = data.map(item => item.path); // Извлекаем ссылки на изображения из JSON
+          })
+          .catch(error => {
+              console.error('Ошибка при загрузке JSON:', error);
+          });
+  }
+
+  // Функция для загрузки изображений
+  function loadImages() {
       const nextImages = images.slice(currentIndex, currentIndex + imagesPerLoad);
       nextImages.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.alt = 'Gallery Image';
-        img.classList.add('gallery-image');
-        imagesContainer.appendChild(img); // Вставляем изображение перед кнопкой
+          const img = document.createElement('img');
+          img.src = src;
+          img.alt = 'Gallery Image';
+          img.classList.add('gallery-image');
+          imagesContainer.appendChild(img); // Добавляем изображение в контейнер
       });
-  
+
       currentIndex += nextImages.length;
-  
+
       // Если изображений больше нет, скрываем кнопку
       if (currentIndex >= images.length) {
-        loadMoreButton.style.display = 'none';
+          loadMoreButton.style.display = 'none';
       }
-    }
-  
-    // Загрузка изображений при нажатии на кнопку
-    loadMoreButton.addEventListener('click', loadImages);
-  
-    // Начальная загрузка
-    loadImages();
-  });
-  
+  }
+
+  // Загрузка изображений при нажатии на кнопку
+  loadMoreButton.addEventListener('click', loadImages);
+
+  // Изначальная загрузка JSON и начальная загрузка изображений
+  loadJson().then(loadImages);
+});
